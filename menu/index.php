8571588dc3e50c8cdd37c3a31f3cf890c60a96aa
vhniii <rasmus.laane@khk.ee>
2019-02-14 10:01:36 +0200

    OOP Menu - Template

--- menu/index.php
+++ menu/index.php
@@ -2,12 +2,62 @@
 
 require_once 'conf.php';
 
-echo '<pre>';
-print_r($sess);
-echo '</pre>';
+//echo '<pre>';
+//print_r($sess);
+//echo '</pre>';
+
+$mainTmpl = new Template('main');
+
+
+$mainTmpl->set('title', 'Menu App');
+$mainTmpl->set("page title", "Tartu KHK Menu");
+
+
+$contentTmpl = new Template('content');
+
+$courseCardTmpl = new Template('course_card');
+
+$courseCardHeaderTmpl = new Template('course_card_header');
+$courseCardDataTmpl = new Template('course_card_data');
 
 
 
+
+$courseNames = array(
+    'praed' => 'fa-utensils',
+    'supid' => 'fa-utensil-spoon',
+    'magustoidud' => 'fa-cookie-bite',
+    'joogid' => 'fa-glass-whiskey');
+
+foreach ($courseNames as $courseName => $courseIcon){
+    $courseCardHeaderTmpl->set('course_name', $courseName);
+    $courseCardHeaderTmpl->set('icon', $courseIcon);
+    $courseCardTmpl->set('course_card_header', $courseCardHeaderTmpl->parse());
+
+    $courseCardDataTmpl->set('course-name', $courseName);
+    $courseCardTmpl->set ('course_card_data', $courseCardDataTmpl->parse());
+
+    $contentTmpl->add('course_cards', $courseCardTmpl->parse());
+}
+
+
+$mainTmpl->set('content', $contentTmpl->parse());
+
+$mainTmplContent = $mainTmpl->parse();
+
+echo $mainTmplContent;
+
+
+
+
+
+//echo $contentTmplContent;
+
+
+
+//echo '<pre>';
+//print_r($mainTmpl);
+//echo '</pre>';
 
 
 //
